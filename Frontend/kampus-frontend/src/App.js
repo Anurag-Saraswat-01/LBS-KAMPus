05f0e881bfe9c4925b6cff23b121bdedbe8fa7d7
Manigandan Kasimani <manigandankasimani@gmail.com>
2022-01-29 11:50:28 +0530

    Merge conflice

--- Frontend/kampus-frontend/src/App.js
+++ Frontend/kampus-frontend/src/App.js
@@ -8,21 +8,21 @@ import AskQuestion from "./pages/AskQuestion";
 import Header from "./components/Header";
 import { useState } from "react";
 function App() {
-  // TODO: Add routing, then use useLocation() hook to get current page
-  // If current page is Landing, set isLanding to true and pass as prop to Header
-  // Alternatively, no need to create state, can just use === /landing or /signup
-  //   const [isLanding, setisLanding] = useState(false);
-  return (
-    <div className="App">
-      {/* <Header page={isLanding} /> */}
-      {/* <Landing /> */}
-      {/* <Profile /> */}
-      {/* <Homepage /> */}
-      <SignUp />
-      {/* <SignIn /> */}
-      {/* <AskQuestion /> */}
-    </div>
-  );
+	// TODO: Add routing, then use useLocation() hook to get current page
+	// If current page is Landing, set isLanding to true and pass as prop to Header
+	// Alternatively, no need to create state, can just use === /landing or /signup
+	//   const [isLanding, setisLanding] = useState(false);
+	return (
+		<div className="App">
+			{/* <Header page={isLanding} /> */}
+			{/* <Landing /> */}
+			{/* <Profile /> */}
+			{/* <Homepage /> */}
+			<SignUp />
+			{/* <SignIn /> */}
+			{/* <AskQuestion /> */}
+		</div>
+	);
 }
 
 export default App;
