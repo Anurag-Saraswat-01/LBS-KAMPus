9474b64773f3a4c0630e11156a93cc6147d98456
Anurag Saraswat <anu01saraswat@gmail.com>
2022-02-06 18:02:06 +0530

    Answer Modal css

--- Frontend/kampus-frontend/src/components/AnswerModal.jsx
+++ Frontend/kampus-frontend/src/components/AnswerModal.jsx
@@ -8,9 +8,10 @@ import Button from "@mui/material/Button";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 // Other themes for the the React-Quill
-import "react-quill/dist/quill.bubble.css";
+// import "react-quill/dist/quill.bubble.css";
 // import "react-quill/dist/quill.core.css";
-import CloseSharpIcon from '@mui/icons-material/CloseSharp';
+import CloseSharpIcon from "@mui/icons-material/CloseSharp";
+import "../css/AnswerModal.css";
 
 const style = {
   position: "absolute",
@@ -25,6 +26,26 @@ const style = {
   p: 4,
 };
 
+const modules = {
+  toolbar: [
+    [{ header: "1" }, { header: "2" }, { font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image", "video"],
+    ["code-block"],
+  ],
+  clipboard: {
+    // toggle to add extra line breaks when pasting HTML:
+    matchVisual: false,
+  },
+};
+
 const AnswerModal = () => {
   // MODAL
   const [open, setOpen] = useState(false);
@@ -36,6 +57,8 @@ const AnswerModal = () => {
     // event.preventDefault();
     setText(value);
   };
+
+  console.log(text);
   return (
     <div className="answerModal">
       <Button onClick={handleOpen}>Open modal</Button>
@@ -53,17 +76,27 @@ const AnswerModal = () => {
         <Fade in={open}>
           <Box sx={style}>
             <div className="answerModal__header">
-            <Typography id="transition-modal-title" variant="h4" component="h2">
-              What is RAFO?
-            </Typography>
-
-						<CloseSharpIcon onClose={handleClose} fontSize="medium" />
-						</div>
-            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Asked By: Anurag Saraswat. On: 24th January, 2022
-            </Typography>
+              <Typography
+                id="transition-modal-title"
+                variant="h4"
+                component="h2"
+              >
+                What is RAFO?
+              </Typography>
+              <CloseSharpIcon className="answerModal__closeIcon" onClick={handleClose} fontSize="medium" />
+            </div>
+            <div className="answerModal__description">
+              <Typography id="transition-modal-description" sx={{ mt: 1, mb: 1 }}>
+                Asked By: Anurag Saraswat. 
+              </Typography>
+              <Typography id="transition-modal-description" sx={{ mt: 1, mb: 1 }}>
+                On: 24th January, 2022
+              </Typography>
+            </div>
             <div className="answerModal__textfield">
               <ReactQuill
+                // formats={formats}
+                modules={modules}
                 value={text}
                 theme="snow"
                 onChange={handleTextChange}
